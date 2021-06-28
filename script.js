a922876d6a180d3fcca92cd278e5701a0f4d1d20
Gustavo Costa <cmtehenz@gmail.com>
2021-06-28 13:22:09 -0300

    :art: _finalizado atividade

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 
-
 function handleShadow(){
+  
   var texto = document.getElementById("boxText").value
-  var txt = texto
+  var txtLow = texto.toLowerCase();
+  var txt = txtLow
       .replace(/i/gi, "inis")
       .replace(/a/gi, "ais")
       .replace(/e/gi, "enter")
       .replace(/o/gi, "omber")
       .replace(/u/gi, "ufter")
-  console.log(txt);
   
+  document.getElementById("boxResult").innerHTML = txt;
+}
+
+function Desofuscar(){
+  var texto = document.getElementById("boxResult").value
+  var txtLow = texto.toLowerCase();
+  var txt = txtLow
+      .replace(/inis/gi,  "i")
+      .replace(/ais/gi,   "a")
+      .replace(/enter/gi, "e")
+      .replace(/omber/gi, "o")
+      .replace(/ufter/gi, "u")
+  
+  document.getElementById("boxText").innerHTML = txt;
+
 }
 
 
